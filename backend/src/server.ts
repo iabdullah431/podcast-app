49a349bc66c065cbe2423da396c1172fa5edce83
abdullah <iaiabidii@gmail.com>
2025-07-15 17:38:04 +0300

    force rebuild: fixed port and logging for Render

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,10 @@ import cors from "cors";
 import searchRoute from "./routes/search.route";
 
 const app = express();
+app.use(cors());
+
 const port = process.env.PORT || 3001;
 
-app.use(cors());
 app.use(express.json());
 
 app.use("/api/search", searchRoute);
