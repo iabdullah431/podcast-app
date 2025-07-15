8594a6cf2f3de0b5606ebba1b5b2528c6c13baf5
abdullah <iaiabidii@gmail.com>
2025-07-15 17:42:44 +0300

    yaaaarb

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -7,10 +7,9 @@ const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const search_route_1 = __importDefault(require("./routes/search.route"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 3001;
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use("/api/search", search_route_1.default);
-app.listen(port, () => {
-    console.log(`✅ Backend API running on port ${port}`);
+app.listen(process.env.PORT || 3001, () => {
+    console.log("✅ Backend API running on port " + (process.env.PORT || 3001));
 });
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,12 +5,10 @@ import searchRoute from "./routes/search.route";
 const app = express();
 app.use(cors());
 
-const port = process.env.PORT || 3001;
-
 app.use(express.json());
 
 app.use("/api/search", searchRoute);
 
-app.listen(port, () => {
-  console.log(`✅ Backend API running on port ${port}`);
+app.listen(process.env.PORT || 3001, () => {
+  console.log("✅ Backend API running on port " + (process.env.PORT || 3001));
 });
